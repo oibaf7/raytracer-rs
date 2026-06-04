3954b2f8b252bdcb21049840e28bf400c5ee814a
Fabio <fabio.cantini.007@gmail.com>
2026-06-04 20:34:14 +0200

    add vector class and initial output file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use raytracer_rs::vector::Color;
+
+mod vector;
+
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3\n {} {} \n255\n",image_width, image_height);
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r =  i as f64 / (image_width - 1) as f64;
+            let g =  j as f64 / (image_height - 1) as f64;
+            let b = 0f64;
+            let color = Color::new(r, g, b);
+
+            println!("{}", color);
+        }
+    }
+
 }
---
+++ src/vector.rs
@@ -0,0 +1,165 @@
+use std::{fmt, ops};
+use std::fmt::Formatter;
+
+#[derive(Default)]
+pub struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self {
+            x,
+            y,
+            z,
+        }
+    }
+
+    pub fn get_x(&self) -> f64 {
+        self.x
+    }
+
+    pub fn get_y(&self) -> f64 {
+        self.y
+    }
+
+    pub fn get_z(&self) -> f64 {
+        self.z
+    }
+
+    pub fn get_length_squared(&self) -> f64 {
+         self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn get_length(&self) -> f64 {
+        self.get_length_squared().sqrt()
+    }
+
+    pub fn dot(&self, rhs: &Vec3) -> f64 {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+
+    pub fn cross(&self, rhs: &Vec3) -> Self {
+        Self {
+            x: self.y * rhs.x - self.z * rhs.y,
+            y: self.z * rhs.x - self.x * rhs.z,
+            z: self.x * rhs.y - self.y * rhs.x,
+        }
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        let len = self.get_length();
+        Vec3 {
+            x: self.x / len,
+            y: self.y / len,
+            z: self.z / len,
+        }
+    }
+
+
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}", self.x, self.y, self.z)
+    }
+}
+
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Self::Output {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::AddAssign<Vec3> for Vec3 {
+    fn add_assign(&mut self, rhs: Vec3) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Self::Output {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl ops::SubAssign<Vec3> for Vec3 {
+    fn sub_assign(&mut self, rhs: Vec3) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Self::Output {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
+    }
+}
+
+impl ops::MulAssign<Vec3> for Vec3 {
+    fn mul_assign(&mut self, rhs: Vec3) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z *= rhs.z;
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self::Output {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+pub struct Color(Vec3);
+pub struct Point3(Vec3);
+
+impl Color {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self(Vec3::new(x, y, z))
+    }
+}
+
+impl Point3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self(Vec3::new(x, y, z))
+    }
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {} {}\n", (self.0.x * 255.999) as u8, (self.0.y * 255.999) as u8, (self.0.z * 255.999) as u8)
+    }
+}
